73cf0310cceba30e8cb0b959b335b13b9bff5b33
AmeurLo <ameur.lourdjane@gmail.com>
2026-04-01 19:05:46 -0400

    Integrate Facebook Pixel tracking script
    Added Facebook Pixel tracking script to layout.

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono, Nunito } from "next/font/google";
 import "./globals.css";
@@ -42,7 +43,19 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             {children}
             <CookieConsent />
           </LanguageProvider>
+          
         </ThemeProvider>
+                <Script
+                            id="meta-pixel"
+                            strategy="afterInteractive"
+                            dangerouslySetInnerHTML={{
+                                          __html: `
+                                                        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+                                                                      fbq('init', '3482648511873376');
+                                                                                    fbq('track', 'PageView');
+                                                                                                `,
+                            }}
+                          />
       </body>
     </html>
   );
